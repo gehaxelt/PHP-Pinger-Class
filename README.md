df4b09b67b2e34415e98705360b91cefb4338f58
Sebastian Neef <github@gehaxelt.in>
2012-07-17 21:40:08 +0200

    Added $timeout to ping() function as wished in issue #1

--- README.md
+++
@@ -1,9 +0,0 @@
-PHP-Pinger-Class
-=====================
-Small php class for pinging a server on a specific port.
-
-See example.php for usage.
-
-Have fun using it.
-
-Gehaxelt
--- phppinger.class.php
+++ phppinger.class.php
@@ -13,12 +13,14 @@ class PHPPinger {
 	 * @param string $host - Domain or IP address
 	 * @param int $port - Port to connect to. Default value: 80
 	 * @param boolean $supressWarning - Supress the warning of fsockopen? Default value: true
+	 * @param int $timeout - Timeout for the fsockopen function. Default value: 30 secs
 	 * @throws NotAStringException
 	 * @throws NotAnIntegerException
 	 * @throws ConnectionFailedException - If ping failed
 	 * @return boolean - If ping succeeded
 	 */
-	public function ping($host,$port=80,$supressWarning=true) {
+	public function ping($host,$port=80,$supressWarning=true,$timeout=30) {
+		
 		if(!is_string($host))
 			throw new NotAStringException('First parameter $host is not a string');
 		
@@ -26,12 +28,15 @@ class PHPPinger {
 			throw new NotAnIntegerException('Second parameter $port is not an integer');
 		
 		if(!is_bool($supressWarning))
-			throw NotABooleanException('Third parameter $supressWarning is not a boolean');
+			throw new NotABooleanException('Third parameter $supressWarning is not a boolean');
+		
+		if(!is_int($timeout))
+			throw new NotAnIntegerException('Fourth paramter $timeout is not a integer');
 		
 		if($supressWarning) {
-			$connection = @fsockopen($host,$port);
+			$connection = @fsockopen($host,$port, $errno, $errstr,$timeout);
 		} else {
-			$connection = fsockopen($host,$port);
+			$connection = fsockopen($host,$port, $errno, $errstr,$timeout);
 		}
 
 		if(!$connection)
